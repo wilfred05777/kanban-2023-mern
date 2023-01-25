fde4e5b58db37521a1da3cf08bd752f1fb79e6eb
Wilfred <wilfred05777@gmail.com>
2023-01-25 08:14:43 +0800

    0:11:00

--- client/src/assets/index.tsx
+++ client/src/assets/index.tsx
@@ -3,7 +3,13 @@ const assets = {
   images:{
     logoDark: require("./images/logo-dark.png"),
     appIcon: require("./images/favicon.png")
+  },
+
+  colors:{
+    secondary: "#292929"
   }
 
 
 }
+
+export default assets;
--- server/app.js
+++ server/app.js
@@ -3,8 +3,8 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const indexRouter = require("./src/v1/routes/index");
+const usersRouter = require("./src/v1/routes/users");
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.use("/api/v1", require("./src/v1/routes"));
 app.use("/users", usersRouter);
 
 module.exports = app;
