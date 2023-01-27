47b1a1a22349b00fe2b062e07a9bf164495e1dea
Wilfred <wilfred05777@gmail.com>
2023-01-28 05:15:18 +0800

    0:35:14 initial component, layout, pages end

---
+++ client/src/components/common/Sidebar.tsx
@@ -0,0 +1,20 @@
+import { Box, CircularProgress } from "@mui/material";
+import React from "react";
+
+const Sidebar = (props: any) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100",
+        height: props.fullHeight
+      }}
+    >
+      <CircularProgress>
+    </Box>
+  );
+};
+
+export default Sidebar;
